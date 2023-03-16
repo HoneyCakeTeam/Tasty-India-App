4ae5ed9794bb554932de845193552e99cbe3772e
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-03-16 22:15:27 +0200

    add setup function to HomeActivity

--- app/src/main/java/com/example/tastyindia/HomeActivity.kt
+++ app/src/main/java/com/example/tastyindia/HomeActivity.kt
@@ -2,10 +2,23 @@ package com.example.tastyindia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tastyindia.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var parser: CsvParser
+    private lateinit var datasource: RecipeDataSource
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setup()
+    }
+
+    private fun setup() {
+        parser = CsvParser()
+        datasource = CsvDataSource(this , parser)
     }
 }

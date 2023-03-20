7bb8e9107b407be9e91efa727907e44ff6ced56c
Narges-Nagy <nargesnagy21@gmail.com>
2023-03-20 23:34:27 +0200

    create recommendation branch from home

--- app/src/main/java/com/example/tastyindia/ui/HomeFragment.kt
+++ app/src/main/java/com/example/tastyindia/ui/HomeFragment.kt
@@ -8,15 +8,11 @@ import com.example.tastyindia.utils.CsvParser
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override val TAG: String="HOME"
-    private lateinit var parser: CsvParser
-    private lateinit var datasource: RecipeDataSource
 
     override fun getViewBinding(): FragmentHomeBinding = FragmentHomeBinding.inflate(layoutInflater)
 
     override fun setUp() {
-        parser = CsvParser()
-        datasource = CsvDataSource(requireContext(), parser)
-    }
+        }
 
     override fun addCallbacks() {
     }

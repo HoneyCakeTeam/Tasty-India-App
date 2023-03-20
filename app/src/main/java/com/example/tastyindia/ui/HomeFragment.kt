ddd3707a0bdabca4f8e00ab23faa85c55aea3ff2
Narges-Nagy <nargesnagy21@gmail.com>
2023-03-20 20:07:11 +0200

    setup data in home screen

--- app/src/main/java/com/example/tastyindia/ui/HomeFragment.kt
+++ app/src/main/java/com/example/tastyindia/ui/HomeFragment.kt
@@ -1,19 +1,27 @@
 package com.example.tastyindia.ui
 
+import com.example.tastyindia.data.source.CsvDataSource
+import com.example.tastyindia.data.source.RecipeDataSource
 import com.example.tastyindia.databinding.FragmentHomeBinding
+import com.example.tastyindia.utils.CsvParser
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override val TAG: String="HOME"
+    private lateinit var parser: CsvParser
+    private lateinit var datasource: RecipeDataSource
 
     override fun getViewBinding(): FragmentHomeBinding = FragmentHomeBinding.inflate(layoutInflater)
 
     override fun setUp() {
-
+        parser = CsvParser()
+        datasource = CsvDataSource(requireContext(), parser)
     }
 
     override fun addCallbacks() {
     }
+
+
 
 
 }

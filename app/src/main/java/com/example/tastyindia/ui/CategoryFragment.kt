9b82f6f14490b72141f907f237d388b65ade3e3a
Mohamed Naser <mahmmedn19@gmail.com>
2023-03-20 22:14:24 +0200

    implement desgine and some fun in fragment categories

--- app/src/main/java/com/example/tastyindia/ui/CategoryFragment.kt
+++ app/src/main/java/com/example/tastyindia/ui/CategoryFragment.kt
@@ -1,23 +1,50 @@
 package com.example.tastyindia.ui
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.example.tastyindia.R
+
+import com.example.tastyindia.data.domain.Recipe
+import com.example.tastyindia.data.source.CsvDataSource
+import com.example.tastyindia.data.source.RecipeDataSource
+import com.example.tastyindia.databinding.FragmentCategoryBinding
+import com.example.tastyindia.utils.CsvParser
 
 
-class CategoryFragment : Fragment() {
+class CategoryFragment : BaseFragment<FragmentCategoryBinding>(){
 
+    private lateinit var parser: CsvParser
+    private lateinit var datasource: RecipeDataSource
+    override val TAG: String = "CategoryFragment"
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false)
+    override fun getViewBinding(): FragmentCategoryBinding = FragmentCategoryBinding.inflate(layoutInflater)
+
+    override fun setUp() {
+        parser = CsvParser()
+        datasource = CsvDataSource(requireContext(), parser)
+
+        val recipe = listOf("Chicken","Fish","Lentils","Millet","Cardamom","Tomatoes","Ginger","Turmeric","Cinnamon","Sweet Potato","Spinach","Spinach","Fenugreek")
+        val list = filterFastRecipes()
+        log(list.toString())
     }
+
+    override fun addCallbacks() {
+
+    }
+
+    fun filterHealthRecipes( health :List<String>):List<Recipe>{
+        return datasource.getAllRecipes().filter {recipe->
+            health.any {
+                recipe.ingredients.contains(it,true)
+            }
+        }
+    }
+    fun filterFastRecipes():List<Int>{
+        return datasource.getAllRecipes().sortedBy{it.totalTime
+        }.map { it.totalTime } }
+
+    fun filterEasyRecipes():List<Recipe>{
+        return datasource.getAllRecipes().sortedBy{it.ingredientsCount
+        }.take(10) }
+
+
 
 
 }

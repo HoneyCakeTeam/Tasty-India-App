17e993d5b00be8f76a7f0a3760f3a87d41e2e3c0
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-03-21 20:52:17 +0200

    add binding

--- app/src/main/java/com/example/tastyindia/ui/KitchenInfoFragment.kt
+++ app/src/main/java/com/example/tastyindia/ui/KitchenInfoFragment.kt
@@ -6,14 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.tastyindia.R
+import com.example.tastyindia.databinding.FragmentKitchenInfoBinding
 
 class KitchenInfoFragment : Fragment() {
+
+    private lateinit var binding: FragmentKitchenInfoBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentKitchenInfoBinding.inflate(inflater, container, false)
         return inflater.inflate(R.layout.fragment_kitchen_info, container, false)
     }
 }

18ca10997bbab4d22300c6dfd27e4b845676814b
Mohamed Ragab <mohamed.ragab.ali.9@gmail.com>
2023-03-21 17:11:34 +0200

    prepare class advicesFragment

---
+++ app/src/main/java/com/example/tastyindia/ui/AdvicesFragment.kt
@@ -0,0 +1,18 @@
+package com.example.tastyindia.ui
+
+import com.example.tastyindia.databinding.FragmentAdvicesBinding
+
+class AdvicesFragment : BaseFragment<FragmentAdvicesBinding>() {
+    override val TAG: String = "AdvicesFragment"
+
+    override fun getViewBinding(): FragmentAdvicesBinding = FragmentAdvicesBinding.inflate(layoutInflater)
+
+    override fun setUp() {
+
+    }
+
+    override fun addCallbacks() {
+
+    }
+
+}

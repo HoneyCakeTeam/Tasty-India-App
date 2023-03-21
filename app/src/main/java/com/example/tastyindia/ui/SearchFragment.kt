bae76304939ca5c41b95cfb6f92414d39dc9a19e
Nour Elden Alaa <nourelden515@gmail.com>
2023-03-21 02:31:40 +0200

    implement SearchFragment.kt

--- app/src/main/java/com/example/tastyindia/ui/SearchFragment.kt
+++ app/src/main/java/com/example/tastyindia/ui/SearchFragment.kt
@@ -1,61 +1,41 @@
 package com.example.tastyindia.ui
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.example.tastyindia.R
+import android.widget.SearchView
 import com.example.tastyindia.data.domain.Recipe
-import com.example.tastyindia.data.source.CsvDataSource
-import com.example.tastyindia.data.source.RecipeDataSource
 import com.example.tastyindia.databinding.FragmentSearchBinding
-import com.example.tastyindia.utils.CsvParser
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>() {
 
-    private lateinit var parser: CsvParser
-    private lateinit var datasource: RecipeDataSource
-
     override val TAG: String = "SearchFragment"
-
-    override fun getViewBinding(): FragmentSearchBinding {
-        TODO("Not yet implemented")
-    }
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
-
-    }
+    override fun getViewBinding() = FragmentSearchBinding.inflate(layoutInflater)
 
     override fun setUp() {
-        parser = CsvParser()
-        datasource =CsvDataSource(requireContext(),parser)
-        val list = getSixCuisineFromDataSet(datasource)
-        val list2 =ex(datasource)
-        log(list2.toString())
-
     }
+
     override fun addCallbacks() {
+        addSearchListener()
     }
 
-    fun getSixCuisineFromDataSet(dataSource: RecipeDataSource):List<String>{
-        return dataSource
-            .getAllRecipes()
-            .map { it.cuisine }
-            .distinct()
-            .take(6)
+    private fun addSearchListener() {
+        binding?.searchBar?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                val result = searchByRecipeOrCuisine(newText!!)
+                log(result.size.toString())
+                return true
+            }
+        })
     }
 
-    fun ex(dataSource: RecipeDataSource ): List<String>{
-        return dataSource
-            .getAllRecipes()
-            .filter { it.cuisine == "Indian" }
-            .map { it.recipeName}
-            .take(10)
+    private fun searchByRecipeOrCuisine(searchWord: String): List<Recipe> {
+        return listOfRecipe
+            .filter {
+                it.cuisine.lowercase().contains(searchWord.lowercase()) || it.recipeName.lowercase()
+                    .contains(searchWord.lowercase())
+            }
     }
 }

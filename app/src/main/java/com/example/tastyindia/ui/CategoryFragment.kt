6e156f326ed7b7c4c1db751d7188967f39185607
Hagar-Gomaa <hagargomaa313@gmail.com>
2023-03-21 02:50:43 +0200

    implement  category fragment

--- app/src/main/java/com/example/tastyindia/ui/CategoryFragment.kt
+++ app/src/main/java/com/example/tastyindia/ui/CategoryFragment.kt
@@ -9,42 +9,36 @@ import com.example.tastyindia.utils.CsvParser
 
 
 class CategoryFragment : BaseFragment<FragmentCategoryBinding>(){
-
-    private lateinit var parser: CsvParser
-    private lateinit var datasource: RecipeDataSource
     override val TAG: String = "CategoryFragment"
 
     override fun getViewBinding(): FragmentCategoryBinding = FragmentCategoryBinding.inflate(layoutInflater)
 
     override fun setUp() {
-        parser = CsvParser()
-        datasource = CsvDataSource(requireContext(), parser)
-
         val recipe = listOf("Chicken","Fish","Lentils","Millet","Cardamom","Tomatoes","Ginger","Turmeric","Cinnamon","Sweet Potato","Spinach","Spinach","Fenugreek")
-        val list = filterFastRecipes()
+        val list = filterHealthyRecipes(recipe)
         log(list.toString())
     }
 
     override fun addCallbacks() {
-
     }
 
-    fun filterHealthRecipes( health :List<String>):List<Recipe>{
-        return datasource.getAllRecipes().filter {recipe->
-            health.any {
-                recipe.ingredients.contains(it,true)
-            }
-        }
+    fun filterHealthyRecipes( health :List<String>):List<String>{
+        return listOfRecipe.filter {excludeUnHealthyRecipes(it,health) }
+            .map { it .ingredients }
     }
     fun filterFastRecipes():List<Int>{
-        return datasource.getAllRecipes().sortedBy{it.totalTime
-        }.map { it.totalTime } }
+        return listOfRecipe.sortedBy{it.totalTime
+                       }.map { it.totalTime } }
 
-    fun filterEasyRecipes():List<Recipe>{
-        return datasource.getAllRecipes().sortedBy{it.ingredientsCount
-        }.take(10) }
+    fun filterEasyRecipes():List<Int>{
+        return listOfRecipe.sortedBy{it.ingredientsCount
+                      }.map { it.ingredientsCount }}
+
+     fun excludeUnHealthyRecipes(recipe:Recipe,health: List<String>):Boolean{
+        return health.any {
+            recipe.ingredients.lowercase().contains(it.lowercase())
+        }}
+
+     }
 
 
-
-
-}

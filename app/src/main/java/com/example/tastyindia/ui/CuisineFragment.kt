9aeec6accdb75ea186d1dc7396475160d5165541
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-03-21 19:18:14 +0200

    add function getAllCuisines in CuisineFragment

--- app/src/main/java/com/example/tastyindia/ui/CuisineFragment.kt
+++ app/src/main/java/com/example/tastyindia/ui/CuisineFragment.kt
@@ -1,19 +1,23 @@
 package com.example.tastyindia.ui
 
-import com.example.tastyindia.databinding.FragmentKitchenBinding
+import com.example.tastyindia.databinding.FragmentCuisineBinding
 
-class CuisineFragment : BaseFragment<FragmentKitchenBinding>(){
+class CuisineFragment : BaseFragment<FragmentCuisineBinding>() {
     override val TAG: String = "CuisineFragment"
-
-    override fun getViewBinding(): FragmentKitchenBinding  = FragmentKitchenBinding.inflate(layoutInflater)
+    override fun getViewBinding(): FragmentCuisineBinding =
+        FragmentCuisineBinding.inflate(layoutInflater)
 
     override fun setUp() {
-
+        log(getAllCuisines().size.toString())
+        getAllRecipes()
     }
 
     override fun addCallbacks() {
 
     }
 
+    private fun getAllCuisines(): List<String> {
+        return getAllRecipes().map{ it.cuisine }.distinct()
+    }
 
 }

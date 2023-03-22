21d38394c6edd0d82de5a95b9d5759fc9a4e7645
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-03-22 16:36:41 +0200

    add function to get RecipeDetails

--- app/src/main/java/com/example/tastyindia/ui/RecipeDetails.kt
+++ app/src/main/java/com/example/tastyindia/ui/RecipeDetails.kt
@@ -1,8 +1,9 @@
 package com.example.tastyindia.ui
 
+import android.util.Log
 import com.example.tastyindia.databinding.FragmentRecipeDetailsBinding
 
-class RecipeDetails() : BaseFragment<FragmentRecipeDetailsBinding>() {
+class RecipeDetails : BaseFragment<FragmentRecipeDetailsBinding>() {
     override val TAG: String
         get() = TODO("Not yet implemented")
 
@@ -11,13 +12,15 @@ class RecipeDetails() : BaseFragment<FragmentRecipeDetailsBinding>() {
     }
 
     override fun setUp() {
-
+        val recipe = getRecipeDetails(1)
+        recipe.recipeName
+        Log.e("TAG", recipe.recipeName)
     }
 
     override fun addCallbacks() {
         TODO("Not yet implemented")
     }
-    fun getRecipeDetails(id:Int){
 
-    }
+    private fun getRecipeDetails(id: Int) = listOfRecipe[id]
+
 }

c195cd454cb0c92fda4eb5a8b450c10220853db8
Mohamed Naser <mahmmedn19@gmail.com>
2023-03-22 17:55:31 +0200

    update code in fragment

--- app/src/main/java/com/example/tastyindia/ui/CategoryFragment.kt
+++ app/src/main/java/com/example/tastyindia/ui/CategoryFragment.kt
@@ -2,43 +2,50 @@ package com.example.tastyindia.ui
 
 
 import com.example.tastyindia.data.domain.Recipe
-import com.example.tastyindia.data.source.CsvDataSource
-import com.example.tastyindia.data.source.RecipeDataSource
 import com.example.tastyindia.databinding.FragmentCategoryBinding
-import com.example.tastyindia.utils.CsvParser
 
 
-class CategoryFragment : BaseFragment<FragmentCategoryBinding>(){
+class CategoryFragment : BaseFragment<FragmentCategoryBinding>() {
     override val TAG: String = "CategoryFragment"
 
-    override fun getViewBinding(): FragmentCategoryBinding = FragmentCategoryBinding.inflate(layoutInflater)
+    override fun getViewBinding(): FragmentCategoryBinding =
+        FragmentCategoryBinding.inflate(layoutInflater)
 
     override fun setUp() {
-        val recipe = listOf("Chicken","Fish","Lentils","Millet","Cardamom","Tomatoes","Ginger","Turmeric","Cinnamon","Sweet Potato","Spinach","Spinach","Fenugreek")
-        val list = filterHealthyRecipes(recipe)
-        log(list.toString())
+        val recipe = listOf(
+            "Chicken", "Fish", "Lentils", "Millet", "Cardamom", "Tomatoes", "Ginger", "Turmeric", "Cinnamon", "Sweet Potato", "Spinach", "Spinach", "Fenugreek"
+        )
+        val listHealthy = filterHealthyRecipes(recipe)
+        log(listHealthy.toString())
+        val listFast = filterFastRecipes()
+        log(listFast.toString())
+        val listEasy = filterEasyRecipes()
+        log(listEasy.toString())
     }
 
     override fun addCallbacks() {
     }
 
-    fun filterHealthyRecipes( health :List<String>):List<String>{
-        return listOfRecipe.filter {excludeUnHealthyRecipes(it,health) }
-            .map { it .ingredients }
+    private fun filterHealthyRecipes(health: List<String>): List<Pair<String, String>> {
+        return listOfRecipe.filter { excludeUnHealthyRecipes(it, health) }
+            .map { Pair(it.recipeName, it.ingredients) }
     }
-    fun filterFastRecipes():List<Int>{
-        return listOfRecipe.sortedBy{it.totalTime
-                       }.map { it.totalTime } }
 
-    fun filterEasyRecipes():List<Int>{
-        return listOfRecipe.sortedBy{it.ingredientsCount
-                      }.map { it.ingredientsCount }}
+    private fun filterFastRecipes(): List<Pair<String, Int>> {
+        return listOfRecipe.sortedBy {
+            it.totalTime
+        }.map { Pair(it.recipeName, it.totalTime) }
+    }
 
-     fun excludeUnHealthyRecipes(recipe:Recipe,health: List<String>):Boolean{
+    private fun filterEasyRecipes(): List<Pair<String, Int>> {
+        return listOfRecipe.sortedBy {
+            it.ingredientsCount
+        }.map { Pair(it.recipeName, it.ingredientsCount) }
+    }
+
+    private fun excludeUnHealthyRecipes(recipe: Recipe, health: List<String>): Boolean {
         return health.any {
             recipe.ingredients.lowercase().contains(it.lowercase())
-        }}
-
-     }
-
-
+        }
+    }
+}

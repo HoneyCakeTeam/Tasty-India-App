ec0c683e2795e3b3bdd983cd56a78f47c64d30c8
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-03-22 12:08:46 +0200

    remove unused object

--- app/src/main/java/com/example/tastyindia/data/domain/KitchenInfo.kt
+++ app/src/main/java/com/example/tastyindia/data/domain/KitchenInfo.kt
@@ -1,8 +1,5 @@
 package com.example.tastyindia.data.domain
 
-import com.example.tastyindia.R
-import com.example.tastyindia.ui.KitchenInfoFragment
-
 data class KitchenInfo(
     var kitchenName: String,
     var kitchenImage: Int,
@@ -10,13 +7,4 @@ data class KitchenInfo(
     var RegionsDescription: String,
     var dishesDescription: String
 )
-
-val indianKitchen = KitchenInfo(
-    "Indian Cuisine",
-    R.drawable.card_image,
-    KitchenInfoFragment().getString(R.string.indian_history),
-    KitchenInfoFragment().getString(R.string.region_desc),
-    KitchenInfoFragment().getString(R.string.indian_dishes)
-)
-
 

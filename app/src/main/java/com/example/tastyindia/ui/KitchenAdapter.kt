bd6cca0aa6f37ecf80c24dd24a93f6eef943d9aa
Nour Elden Alaa <nourelden515@gmail.com>
2023-03-23 23:27:27 +0200

    enhance the code

---
+++ app/src/main/java/com/example/tastyindia/ui/KitchenAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.tastyindia.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.example.tastyindia.R
+import com.example.tastyindia.data.domain.Recipe
+import com.example.tastyindia.databinding.KitchenItemBinding
+
+class KitchenAdapter(private val kitchenList: List<Recipe>) :
+    RecyclerView.Adapter<KitchenAdapter.KitchenViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KitchenViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_kitchen, parent,false)
+        return KitchenViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: KitchenViewHolder, position: Int) {
+        val currentKitchen = kitchenList[position]
+
+        holder.binding.apply {
+            textKitchenName.text = currentKitchen.cuisine
+            Glide
+                .with(holder.binding.root)
+                .load(currentKitchen.imageUrl)
+                .placeholder(R.drawable.ic_error)
+                .into(imageKitchen)
+        }
+    }
+
+    override fun getItemCount() = kitchenList.size
+
+    class KitchenViewHolder(viewItem: View) : ViewHolder(viewItem) {
+        val binding = KitchenItemBinding.bind(itemView)
+    }
+}
+
---
+++ app/src/main/java/com/example/tastyindia/ui/KitchenFragment.kt
@@ -0,0 +1,28 @@
+package com.example.tastyindia.ui
+
+import com.example.tastyindia.data.domain.Recipe
+import com.example.tastyindia.databinding.FragmentKitchenBinding
+
+class KitchenFragment : BaseFragment<FragmentKitchenBinding>() {
+
+    private lateinit var adapter: KitchenAdapter
+    override val TAG: String = "CuisineFragment"
+
+    override fun getViewBinding(): FragmentKitchenBinding =
+        FragmentKitchenBinding.inflate(layoutInflater)
+
+    override fun setUp() {
+
+        adapter = KitchenAdapter(getAllCuisines())
+        binding?.rvKitchen?.adapter = adapter
+    }
+
+    override fun addCallbacks() {
+
+    }
+
+    private fun getAllCuisines():List<Recipe>{
+        return listOfRecipe.distinctBy { it.cuisine }
+    }
+
+}

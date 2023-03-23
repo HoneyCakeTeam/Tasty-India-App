27aceb5e5d986bf881f13b99f6ad269e444ce804
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-03-23 18:00:00 +0200

    update KitchenFragment to add adapter

--- app/src/main/java/com/example/tastyindia/ui/KitchenFragment.kt
+++ app/src/main/java/com/example/tastyindia/ui/KitchenFragment.kt
@@ -1,20 +1,29 @@
 package com.example.tastyindia.ui
 
+import com.example.tastyindia.adapters.KitchenAdapter
+import com.example.tastyindia.data.domain.Recipe
 import com.example.tastyindia.databinding.FragmentKitchenBinding
 
 class KitchenFragment : BaseFragment<FragmentKitchenBinding>() {
+
+    private lateinit var adapter: KitchenAdapter
     override val TAG: String = "CuisineFragment"
+
     override fun getViewBinding(): FragmentKitchenBinding =
         FragmentKitchenBinding.inflate(layoutInflater)
 
     override fun setUp() {
-        log(getAllCuisines().size.toString())
+
+        adapter = KitchenAdapter(getAllCuisines())
+        binding?.rvKitchen?.adapter = adapter
     }
 
     override fun addCallbacks() {
 
     }
 
-    private fun getAllCuisines() = listOfRecipe.map { it.cuisine }.distinct()
+    private fun getAllCuisines():List<Recipe>{
+        return listOfRecipe.distinctBy { it.cuisine }
+    }
 
 }

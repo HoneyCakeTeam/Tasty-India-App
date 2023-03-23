8e71411140552ab61a97206686f47a3ed536324f
Aziza <aziza.helmy35@gmail.com>
2023-03-24 01:49:58 +0200

    add CategoryDetailsFragment.kt

---
+++ app/src/main/java/com/example/tastyindia/ui/CategoryDetailsFragment.kt
@@ -0,0 +1,21 @@
+package com.example.tastyindia.ui
+
+import com.example.tastyindia.databinding.FragmentCategoryDetailsBinding
+
+class CategoryDetailsFragment : BaseFragment<FragmentCategoryDetailsBinding>() {
+    override val TAG: String
+        get() = TODO("Not yet implemented")
+
+    override fun getViewBinding(): FragmentCategoryDetailsBinding =
+        FragmentCategoryDetailsBinding.inflate(layoutInflater)
+
+    override fun setUp() {
+        TODO("Not yet implemented")
+    }
+
+    override fun addCallbacks() {
+        TODO("Not yet implemented")
+    }
+
+
+}

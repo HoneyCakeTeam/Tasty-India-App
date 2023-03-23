c6a086625c141be690b7802af3a19d2f27af9809
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-03-23 21:17:09 +0200

    add function setAppBar in BaseFragment

--- app/src/main/java/com/example/tastyindia/ui/BaseFragment.kt
+++ app/src/main/java/com/example/tastyindia/ui/BaseFragment.kt
@@ -5,8 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
+import com.example.tastyindia.R
 import com.example.tastyindia.data.domain.Recipe
 import com.example.tastyindia.data.source.CsvDataSource
 import com.example.tastyindia.data.source.RecipeDataSource
@@ -60,5 +64,29 @@ abstract class BaseFragment<VB : ViewBinding> : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    protected fun setAppBar(
+        visibility: Boolean,
+        title: String? = null,
+        showNavigationIcon: Boolean = true
+    ) {
+        val toolbar: ConstraintLayout? = activity?.findViewById(R.id.toolbarContainer)
+        val pageTitle: TextView? = activity?.findViewById(R.id.text_pageTitle)
+        val navigateIcon: ImageButton? = activity?.findViewById(R.id.button_navDirection)
+
+        if (visibility) {
+            toolbar?.visibility = View.VISIBLE
+            title?.let {
+                pageTitle?.text = title
+            }
+            if (showNavigationIcon) {
+                navigateIcon?.visibility = View.VISIBLE
+            } else {
+                navigateIcon?.visibility = View.GONE
+            }
+        } else {
+            toolbar?.visibility = View.GONE
+        }
     }
 }

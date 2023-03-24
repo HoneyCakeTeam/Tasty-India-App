c4eab2d00c844b35d7949ee9e5176535ece12eae
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-03-24 06:45:21 +0200

    add 2 variables to get arguments from another fragment

--- app/src/main/java/com/example/tastyindia/ui/KitchenInfoFragment.kt
+++ app/src/main/java/com/example/tastyindia/ui/KitchenInfoFragment.kt
@@ -6,13 +6,21 @@ import com.example.tastyindia.utils.Constants.Key.KITCHEN_IMAGE_URL
 import com.example.tastyindia.utils.Constants.Key.KITCHEN_NAME
 
 class KitchenInfoFragment : BaseFragment<FragmentKitchenInfoBinding>() {
+
     override val TAG: String = "KITCHEN_INFO"
+    private lateinit var kitchenName: String
+    private lateinit var kitchenImageUrl: String
 
     override fun getViewBinding(): FragmentKitchenInfoBinding =
         FragmentKitchenInfoBinding.inflate(layoutInflater)
 
     override fun setUp() {
         setUpAppBar(visibility = true, title = "Kitchen Info", showBackIcon = true)
+
+        arguments?.let {
+            kitchenName = it.getString(KITCHEN_NAME)!!
+            kitchenImageUrl = it.getString(KITCHEN_IMAGE_URL)!!
+        }
     }
 
     override fun addCallbacks() {

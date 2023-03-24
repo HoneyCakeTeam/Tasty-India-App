92176060afb3928658d553c378ba7af8cf0a63e8
Aziza <aziza.helmy35@gmail.com>
2023-03-24 15:36:29 +0200

    get data from previous screen

--- app/src/main/java/com/example/tastyindia/ui/CategoryDetailsFragment.kt
+++ app/src/main/java/com/example/tastyindia/ui/CategoryDetailsFragment.kt
@@ -2,16 +2,25 @@ package com.example.tastyindia.ui
 
 import com.example.tastyindia.data.domain.Recipe
 import com.example.tastyindia.databinding.FragmentCategoryDetailsBinding
+import com.example.tastyindia.utils.Constants
 
 class CategoryDetailsFragment : BaseFragment<FragmentCategoryDetailsBinding>(),
     RecipesAdapter.RecipeInteractionListener {
+
     private lateinit var recipeAdapter: RecipesAdapter
+    private lateinit var categoryName: String
+    private lateinit var categoryImage: String
 
     override val TAG: String = this::class.simpleName.toString()
     override fun getViewBinding(): FragmentCategoryDetailsBinding =
         FragmentCategoryDetailsBinding.inflate(layoutInflater)
 
     override fun setUp() {
+        //TODO check args first then show data
+        arguments?.let {
+            categoryName = it.getString(Constants.Key.CATEGORY_NAME).toString()
+            categoryImage = it.getString(Constants.Key.CATEGORY_IMAGE).toString()
+        }
         initRecyclerView()
         recipeAdapter.setData(getAllRecipes() as ArrayList<Recipe>)
     }
@@ -21,7 +30,9 @@ class CategoryDetailsFragment : BaseFragment<FragmentCategoryDetailsBinding>(),
         binding.rvRecipe.adapter = recipeAdapter
     }
 
-    override fun addCallbacks() {}
+    override fun addCallbacks() {
+        //TODO handle back btn
+    }
 
     override fun onClickItem(recipe: Recipe) {
         RecipeDetailsFragment.newInstance(recipe.recipeName, recipe.imageUrl)

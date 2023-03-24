3cbf420b67b18e2564b8859f0ec7c4ca85e6d219
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-03-24 16:39:49 +0200

    enhance kitchen Details Fragment

--- app/src/main/java/com/example/tastyindia/ui/KitchenDetailsFragment.kt
+++ app/src/main/java/com/example/tastyindia/ui/KitchenDetailsFragment.kt
@@ -1,18 +1,25 @@
 package com.example.tastyindia.ui
 
 import android.os.Bundle
-import com.example.tastyindia.databinding.FragmentCuisineDetailsBinding
+import com.example.tastyindia.data.domain.Recipe
+import com.example.tastyindia.databinding.FragmentKitchenDetailsBinding
 import com.example.tastyindia.utils.Constants.Key.KITCHEN_IMAGE_URL
 import com.example.tastyindia.utils.Constants.Key.KITCHEN_NAME
 
-class KitchenDetailsFragment : BaseFragment<FragmentCuisineDetailsBinding>() {
+class KitchenDetailsFragment : BaseFragment<FragmentKitchenDetailsBinding>() {
+
     override val TAG: String = "CUISINEDETAILS"
+    private lateinit var recipeAdapter: KitchenDetailsAdapter
     private lateinit var kitchenName: String
     private lateinit var kitchenImageUrl: String
-    override fun getViewBinding() = FragmentCuisineDetailsBinding.inflate(layoutInflater)
+
+    override fun getViewBinding() = FragmentKitchenDetailsBinding.inflate(layoutInflater)
 
     override fun setUp() {
+        setUpAppBar(true, kitchenName, true)
         arguments?.let {
+            recipeAdapter = KitchenDetailsAdapter(getRecipeName())
+            binding.rvRecipe.adapter = recipeAdapter
             kitchenName = it.getString(KITCHEN_NAME).toString()
             kitchenImageUrl = it.getString(KITCHEN_IMAGE_URL).toString()
         }
@@ -23,12 +30,15 @@ class KitchenDetailsFragment : BaseFragment<FragmentCuisineDetailsBinding>() {
         TODO("Not yet implemented")
     }
 
-    private fun getRecipeName(): List<String> {
-        return listOfRecipe
+    private fun getRecipeName(): List<Recipe> {
+        return listOfRecipe.distinctBy { it.cuisine == kitchenName }
+
+      /*  return listOfRecipe
             .filter { it.cuisine == kitchenName }
             .map { it.recipeName }
             .take(10)
-    }
+*/    }
+
 
     companion object {
         fun newInstance(kitchenName: String, kitchenImageUrl: String) =

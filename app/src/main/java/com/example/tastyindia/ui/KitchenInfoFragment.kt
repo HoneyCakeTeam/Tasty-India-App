ee33bead696f6c476b4680e616edbf445ec1d66a
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-03-24 06:38:09 +0200

    add newInstance function

--- app/src/main/java/com/example/tastyindia/ui/KitchenInfoFragment.kt
+++ app/src/main/java/com/example/tastyindia/ui/KitchenInfoFragment.kt
@@ -1,6 +1,9 @@
 package com.example.tastyindia.ui
 
+import android.os.Bundle
 import com.example.tastyindia.databinding.FragmentKitchenInfoBinding
+import com.example.tastyindia.utils.Constants.Key.KITCHEN_IMAGE_URL
+import com.example.tastyindia.utils.Constants.Key.KITCHEN_NAME
 
 class KitchenInfoFragment : BaseFragment<FragmentKitchenInfoBinding>() {
     override val TAG: String = "KITCHEN_INFO"
@@ -14,5 +17,15 @@ class KitchenInfoFragment : BaseFragment<FragmentKitchenInfoBinding>() {
 
     override fun addCallbacks() {
 
+    }
+
+    companion object {
+        fun newInstance(kitchenName: String, kitchenImageURL: String) =
+            KitchenInfoFragment().apply {
+                arguments = Bundle().apply {
+                    putString(KITCHEN_NAME, kitchenName)
+                    putString(KITCHEN_IMAGE_URL, kitchenImageURL)
+                }
+            }
     }
 }

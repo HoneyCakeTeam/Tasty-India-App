0b31d5349365a3b34a185b8a08e79370068e878f
Nour Elden Alaa <nourelden515@gmail.com>
2023-03-24 19:20:38 +0200

    refactor SeeAllRecipesFragment.kt

--- app/src/main/java/com/example/tastyindia/ui/seeall/SeeAllRecipesFragment.kt
+++ app/src/main/java/com/example/tastyindia/ui/seeall/SeeAllRecipesFragment.kt
@@ -1,5 +1,6 @@
 package com.example.tastyindia.ui.seeall
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.tastyindia.R
@@ -9,11 +10,14 @@ import com.example.tastyindia.databinding.FragmentSeeAllRecipesBinding
 import com.example.tastyindia.ui.BaseFragment
 import com.example.tastyindia.ui.kitchendetails.KitchenDetailsFragment
 import com.example.tastyindia.ui.RecipeInteractionListener
+import com.example.tastyindia.ui.categorydetails.RecipesAdapter
 import com.example.tastyindia.ui.recipedetails.RecipeDetailsFragment
+import com.example.tastyindia.utils.Constants.Key.RECIPE_NAME
 import com.google.android.material.snackbar.Snackbar
 
 class SeeAllRecipesFragment : BaseFragment<FragmentSeeAllRecipesBinding>(),
     RecipeInteractionListener {
+    private lateinit var recipeList: RecipeList
     private lateinit var adapter: SeeAllRecipesAdapter
     override val TAG = this::class.simpleName.toString()
 
@@ -21,12 +25,23 @@ class SeeAllRecipesFragment : BaseFragment<FragmentSeeAllRecipesBinding>(),
         FragmentSeeAllRecipesBinding.inflate(layoutInflater)
 
     override fun setUp() {
-        adapter = SeeAllRecipesAdapter(listOf(), this)
+        adapter = SeeAllRecipesAdapter(getRecipesList().recipeList, this)
         binding.rvRecipes.adapter = adapter
     }
 
     override fun addCallbacks() {
 
+    }
+
+    private fun getRecipesList(): RecipeList {
+        arguments?.let {
+            recipeList = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                it.getParcelable(RECIPE_NAME, recipeList::class.java)!!
+            } else {
+                it.getParcelable(RECIPE_NAME)!!
+            }
+        }
+        return recipeList
     }
 
     companion object {

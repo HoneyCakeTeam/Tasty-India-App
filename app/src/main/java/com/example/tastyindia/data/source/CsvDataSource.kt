405314efb633d32bb34650f378bb0f5714eaf6ee
Aziza <aziza.helmy35@gmail.com>
2023-03-24 02:21:10 +0200

    fix name issue

--- app/src/main/java/com/example/tastyindia/data/source/CsvDataSource.kt
+++ app/src/main/java/com/example/tastyindia/data/source/CsvDataSource.kt
@@ -3,6 +3,7 @@ package com.example.tastyindia.data.source
 import android.content.Context
 import com.example.tastyindia.data.domain.Recipe
 import com.example.tastyindia.utils.Constants
+import com.example.tastyindia.utils.Constants.Name.CSV_FILE_NAME
 import com.example.tastyindia.utils.CsvParser
 import java.io.BufferedReader
 import java.io.InputStreamReader
@@ -12,7 +13,7 @@ class CsvDataSource(private val context: Context, private val parser: CsvParser)
     override fun getAllRecipes(): List<Recipe> {
         val recipeList = mutableListOf<Recipe>()
         context.apply {
-            val inputStream = assets.open(Constants.FILE_NAME)
+            val inputStream = assets.open(CSV_FILE_NAME)
             val buffer = BufferedReader(InputStreamReader(inputStream))
             buffer.forEachLine {
                 val currentRecipe = parser.parseLine(it)

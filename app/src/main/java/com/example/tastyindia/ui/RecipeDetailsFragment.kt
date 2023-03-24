e716fdd4a926ea1472044b40a4fe59932e9da271
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-03-24 17:39:11 +0200

    implement recycler view

--- app/src/main/java/com/example/tastyindia/ui/RecipeDetailsFragment.kt
+++ app/src/main/java/com/example/tastyindia/ui/RecipeDetailsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.tastyindia.ui
 
+import com.example.tastyindia.data.domain.Recipe
 import com.example.tastyindia.databinding.FragmentRecipeDetailsBinding
 
 class RecipeDetailsFragment : BaseFragment<FragmentRecipeDetailsBinding>() {
@@ -7,16 +8,36 @@ class RecipeDetailsFragment : BaseFragment<FragmentRecipeDetailsBinding>() {
 
     override fun getViewBinding() = FragmentRecipeDetailsBinding.inflate(layoutInflater)
 
+
     override fun setUp() {
         val recipe = getRecipeDetails(1)
         val recipeName = recipe.recipeName
         log(recipeName)
+
+        val ingredientsList = getIngredients(recipe)
+        val instructionsList = getInstructions(recipe)
+
+        val ingredientsAdapter = IngredientsAdapter(ingredientsList)
+        val instructionsAdapter = InstructionsAdapter(instructionsList)
+
+        binding.rvIngredients.adapter = ingredientsAdapter
+        binding.rvInstructions.adapter = instructionsAdapter
+
+        setUpAppBar(true,"",true)
     }
+
 
     override fun addCallbacks() {
         TODO("Not yet implemented")
     }
 
     private fun getRecipeDetails(id: Int) = listOfRecipe[id]
+
+    private fun getIngredients(recipe: Recipe): List<String> {
+        return recipe.ingredients.split(";")
+    }
+    private fun getInstructions(recipe: Recipe): List<String> {
+        return recipe.instructions.split(".").map { it.trim() }
+    }
 
 }

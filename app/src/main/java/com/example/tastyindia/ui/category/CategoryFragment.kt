be9f60a00633d83f01eaada023d5a8ecc0a6b897
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-03-26 14:13:26 +0200

    add image url to string file

--- app/src/main/java/com/example/tastyindia/ui/category/CategoryFragment.kt
+++ app/src/main/java/com/example/tastyindia/ui/category/CategoryFragment.kt
@@ -1,7 +1,7 @@
 package com.example.tastyindia.ui.category
 
-
-import androidx.core.content.ContextCompat
+import android.view.View
+import android.widget.TextView
 import com.example.tastyindia.R
 import com.example.tastyindia.data.DataManager
 import com.example.tastyindia.data.DataManagerInterface
@@ -11,9 +11,10 @@ import com.example.tastyindia.databinding.FragmentCategoryBinding
 import com.example.tastyindia.ui.BaseFragment
 import com.example.tastyindia.ui.category.categoryAdapters.*
 import com.example.tastyindia.utils.CsvParser
+import com.google.android.material.snackbar.Snackbar
 
-
-class CategoryFragment : BaseFragment<FragmentCategoryBinding>(),BaseCategoryInteractionListener {
+class CategoryFragment : BaseFragment<FragmentCategoryBinding>(), BaseCategoryInteractionListener,
+    View.OnClickListener {
     private lateinit var dataSource: CsvDataSource
     private lateinit var dataManager: DataManagerInterface
     override val TAG: String = this::class.simpleName.toString()
@@ -33,27 +34,31 @@ class CategoryFragment : BaseFragment<FragmentCategoryBinding>(),BaseCategoryInt
 
         val itemList: MutableList<CategoryItem<Any>> = mutableListOf()
         itemList.add(
-            CategoryItem(ContextCompat.getDrawable(requireContext(), R.drawable.image_recipe).toString()
-                , CategoryItemType.TYPE_POSTER_IMAGE))
+            CategoryItem(
+                getString(R.string.category_image_poster),
+                CategoryItemType.TYPE_POSTER_IMAGE
+            )
+        )
         itemList.add(CategoryItem(healthyFoodList, CategoryItemType.TYPE_HEALTHY_CATEGORY))
         itemList.add(CategoryItem(fastFoodList, CategoryItemType.TYPE_FAST_CATEGORY))
         itemList.add(CategoryItem(easyFoodList, CategoryItemType.TYPE_EASY_CATEGORY))
 
         val adapter = CategoryMainAdapter(itemList, this)
         binding.rvMain.adapter = adapter
+
     }
 
     override fun onClickRecipe(recipe: Recipe) {
-
+        Snackbar.make(binding.root, "recipe name : ${recipe.recipeName}", Snackbar.LENGTH_LONG)
+            .show()
     }
 
-//    override fun onClick(view: View?) {
-//        when (view) {
-//            binding.seeAllFastFood -> {}
-//            binding.seeAllEasy -> {}
-//            binding.seeAllHealth -> {}
-//
-//        }
-//    }
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.see_all_health -> {Snackbar.make(binding.root, "health", Snackbar.LENGTH_LONG).show()}
+            R.id.see_all_fast -> {Snackbar.make(binding.root, "Fast", Snackbar.LENGTH_LONG).show()}
+            R.id.see_all_easy -> {Snackbar.make(binding.root, "Easy", Snackbar.LENGTH_LONG).show()}
+        }
+    }
 
 }

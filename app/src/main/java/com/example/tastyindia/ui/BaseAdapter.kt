80db69ddf923db4125bce3b78bbf606e16e7d4fd
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-03-27 21:58:28 +0200

    update BaseAdapter

--- app/src/main/java/com/example/tastyindia/ui/BaseAdapter.kt
+++ app/src/main/java/com/example/tastyindia/ui/BaseAdapter.kt
@@ -9,6 +9,7 @@ import androidx.viewbinding.ViewBinding
 interface BaseInteractionListener
 
 abstract class BaseAdapter<T, VB : ViewBinding>(
+
     private var items: List<T>,
     private val listener: BaseInteractionListener
 ) : RecyclerView.Adapter<BaseAdapter.BaseViewHolder<VB>>() {
@@ -18,24 +19,26 @@ abstract class BaseAdapter<T, VB : ViewBinding>(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<VB> {
         val inflater = LayoutInflater.from(parent.context)
         val binding = bindingInflater(inflater, parent, false)
-        return ItemViewHolder(binding)
+        return BaseViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: BaseViewHolder<VB>, position: Int) {
         val item = items[position]
         onBindViewHolder(holder, position, item)
     }
+
     abstract fun onBindViewHolder(holder: BaseViewHolder<VB>, position: Int, item: T)
     override fun getItemCount() = items.size
 
-    class ItemViewHolder<VB : ViewBinding>(binding: VB) : BaseViewHolder<VB>(binding)
-    abstract class BaseViewHolder<VB : ViewBinding>(val binding: VB) : RecyclerView.ViewHolder(binding.root)
+    class BaseViewHolder<VB : ViewBinding>(val binding: VB) : RecyclerView.ViewHolder(binding.root)
 
     open fun setItems(newItems: List<T>) {
-        val diffResult = DiffUtil.calculateDiff(BaseDiffUtil(items, newItems, ::areItemsSame, ::areContentSame))
+        val diffResult =
+            DiffUtil.calculateDiff(BaseDiffUtil(items, newItems, ::areItemsSame, ::areContentSame))
         items = newItems
         diffResult.dispatchUpdatesTo(this)
     }
+
     open fun areItemsSame(oldItem: T, newItem: T) = oldItem?.equals(newItem) == true
     open fun areContentSame(oldPosition: T, newPosition: T) = true
 }
